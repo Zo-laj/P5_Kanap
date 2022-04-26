fd1124993c8a1532d39fcc6a3ec7ecae826947b8
zo-laj <zoe.lajoie2@gmail.com>
2022-04-26 15:57:34 +0200

    insertion des produits dans la page d'accueil

--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,30 @@
-let vareefez = "je suis une tortue";
+const itemsSection = document.getElementById("items");
 
-console.log(vareefez);
+let items = [];
+
+const fetchItems = async () => {
+  await fetch("http://localhost:3000/api/products")
+    .then((res) => res.json())
+    .then((data) => (items = data));
+};
+
+const itemsDisplay = async () => {
+  await fetchItems();
+
+  itemsSection.innerHTML = items
+    .map(
+      (item) =>
+        `
+          <a href="./product.html?id=${item._id}">
+              <article>
+                <img src=${item.imageUrl} alt=${item.altTxt}>
+                <h3 class="productName">${item.name}</h3>
+                <p class="productDescription">${item.description}</p>
+              </article>
+            </a>
+        `
+    )
+    .join(" ");
+};
+
+itemsDisplay();
